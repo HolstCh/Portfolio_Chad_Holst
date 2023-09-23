4138a582021bcae4fe34b797911db477af9de6e5
HolstCh <chad.holst@Hotmail.com>
2023-09-23 15:05:45 -0600

    Home.js buttons are now mobile responsive

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -16,10 +16,10 @@ const Home = () => {
         <section id="home">
             <div className="responsive-width mx-auto flex px-10 py-5 md:flex-row flex-col items-center">
                 <div className="landing-text lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-blue-600">
+                    <h1 className="title-font sm:text-4xl text-2xl mb-4 font-medium text-blue-600">
                         Hi, my name is Chad.<br/>
                     </h1>
-                    <TypeAnimation className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-800 whitespace-nowrap "
+                    <TypeAnimation className="title-font sm:text-4xl text-2xl mb-4 font-medium text-gray-800 whitespace-nowrap "
                         sequence={[
                             "I'm a mechanic.",
                             1000,
@@ -37,27 +37,27 @@ const Home = () => {
                         speed={40}
                         repeat={Infinity}
                     />
-                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-blue-600">
+                    <h1 className="title-font sm:text-4xl text-2xl mb-4 font-medium text-blue-600">
                         Welcome to my coding journey.<br/>
                     </h1>
                     <p className="text-gray-500 mb-8 leading-relaxed text-xl md:w-full w-3/4">
                         Starting in the heavy equipment industry, I have transitioned into the captivating world of software engineering. My experiences as an apprentice have instilled in me: problem-solving abilities, a collaborative skill set, and a keen eye for detail. Such qualities facilitate my commitment to crafting innovative software solutions for society. Let's innovate together!
                     </p>
-                    <div className="flex justify-center">
+                    <div className="flex flex-wrap justify-center">
                         <a
                             href="/Resume_Chad_Holst.pdf"
                             download="Resume_Chad_Holst.pdf"
-                            className="inline-flex text-white bg-blue-600 border-0 py-2 px-6 focus:outline-none hover:bg-blue-400 hover:text-slate-800 rounded text-lg">
+                            className="mb-4 inline-flex text-white bg-blue-600 border-0 py-2 px-6 focus:outline-none hover:bg-blue-400 hover:text-slate-800 rounded text-lg">
                             Download Resume
                         </a>
                         <Link
                             to="/projects"
-                            className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-blue-600 rounded text-lg">
+                            className="mb-4 md:ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-blue-600 rounded text-lg">
                             View My Work
                         </Link>
                         <Link
                             to="/contact"
-                            className="inline-flex text-white bg-blue-600 border-0 ml-4 py-2 px-6 focus:outline-none hover:bg-blue-400 hover:text-slate-800 rounded text-lg">
+                            className="mb-4 inline-flex text-white bg-blue-600 border-0 md:ml-4 py-2 px-6 focus:outline-none hover:bg-blue-400 hover:text-slate-800 rounded text-lg">
                             Let's Collaborate
                         </Link>
                     </div>
