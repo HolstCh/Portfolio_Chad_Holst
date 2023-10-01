2c95d02da5ae663d8f460735f68cdfcaecb8b8bb
HolstCh <chad.holst@Hotmail.com>
2023-10-01 15:59:57 -0600

    changed color of photo navigation arrows

--- my-app/src/components/photoGallery.js
+++ my-app/src/components/photoGallery.js
@@ -25,7 +25,7 @@ const PhotoGallery = ({ photos }) => {
                 </p>
                 <div className="flex items-center justify-center">
                     <button
-                        className="md:mx-8 mx-3 mb-4 inline-flex text-white bg-blue-600 border-0 py-2 px-6 focus:outline-none hover:bg-blue-400 hover:text-slate-800 rounded text-l"
+                        className="md:mx-8 mx-3 mb-4 inline-flex text-gray-400 bg-gray-900 border-0 py-2 px-6 focus:outline-none hover:bg-blue-400 hover:text-slate-800 rounded text-l"
                         onClick={prevImage}
                     >
                         &lt;
@@ -36,7 +36,7 @@ const PhotoGallery = ({ photos }) => {
                         className="sm:w-80 sm:h-80 w-40 h-40 object-cover"
                     />
                     <button
-                        className="md:mx-8 mx-3 mb-4 inline-flex text-white bg-blue-600 border-0 py-2 px-6 focus:outline-none hover:bg-blue-400 hover:text-slate-800 rounded text-l"
+                        className="md:mx-8 mx-3 mb-4 inline-flex text-gray-400 bg-gray-900 border-0 py-2 px-6 focus:outline-none hover:bg-blue-400 hover:text-slate-800 rounded text-l"
                         onClick={nextImage}
                     >
                         &gt;
