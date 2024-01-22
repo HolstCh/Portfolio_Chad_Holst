62fdd02017ac7c5685620222143fa317a65af818
HolstCh <chad.holst@Hotmail.com>
2024-01-21 20:53:17 -0700

    styles for each course listed are responsive

--- my-app/src/components/Courses.js
+++ my-app/src/components/Courses.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const Courses = () => {
     const courses = [
@@ -19,20 +19,55 @@ const Courses = () => {
         "Programming Fundamentals",
         "Operating Systems",
         "Embedded Systems Interfacing",
-        "Digital Circuits"
+        "Computer Organization"
     ];
 
-    return (
-        <div className="grid lg:grid-cols-3 gap-4 mt-4">
-            {courses.map((course, index) => (
-                <div key={index} className={`py-2 px-6 rounded-md shadow-md
+    const [screenSize, setScreenSize] = useState(getScreenSize());
+
+    useEffect(() => {
+        const updateScreenSize = () => {
+            setScreenSize(getScreenSize());
+        };
+        // update screen size when component mounts
+        window.addEventListener('resize', updateScreenSize);
+        // clean up the event listener when component unmounts
+        return () => {
+            window.removeEventListener('resize', updateScreenSize);
+        };
+    }, []);
+
+    function getScreenSize() {
+        return window.innerWidth >= 1024 ? 'lg' : window.innerWidth >= 640 ? 'md' : 'sm';
+    }
+
+    if(screenSize === 'lg')
+    {
+        return (
+            <div className="grid lg:grid-cols-3 sm:grid-cols-1 md:grid-cols-2 gap-4 mt-4">
+                {courses.map((course, index) => (
+                    <div key={index} className={`py-2 px-6 rounded-md shadow-md
                  ${index % 3 === 0 || index % 3 === 2 ? 'bg-blue-600' : 'bg-gray-800'}`}>
-                <h3 className={`font-bold text-lg mb-2 text-center
+                        <h3 className={`font-bold text-lg mb-2 text-center
                  ${index % 3 === 0 || index % 3 === 2 ? 'text-white' : 'text-gray-400'}`}>{course}</h3>
-                </div>
+                    </div>
                 ))}
-        </div>
-    );
+            </div>
+        );
+    }
+    else
+    {
+        return (
+            <div className="grid lg:grid-cols-3 sm:grid-cols-1 md:grid-cols-2 gap-4 mt-4">
+                {courses.map((course, index) => (
+                    <div key={index} className={`py-2 px-6 rounded-md shadow-md
+                 ${index % 2 === 0 ? 'bg-blue-600' : 'bg-gray-800'}`}>
+                        <h3 className={`font-bold text-lg mb-2 text-center
+                 ${index % 2 === 0 ? 'text-white' : 'text-gray-400'}`}>{course}</h3>
+                    </div>
+                ))}
+            </div>
+        );
+    }
 };
 
 export default Courses;
