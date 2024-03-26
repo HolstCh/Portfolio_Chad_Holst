41a31b239ba4ae4a2f4b9277ab9fe5be812352f3
HolstCh <chad.holst@Hotmail.com>
2024-03-26 00:03:58 -0600

    Improves responsiveness: adjust button sizes and flex behavior

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -15,12 +15,12 @@ const Home = () => {
 
     return(
         <section id="home">
-            <div className="responsive-width mx-auto flex px-10 py-5 md:flex-row flex-col items-center">
+            <div className="mx-auto flex px-10 py-5 md:flex-row flex-col items-center">
                 <div className="landing-text lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                     <h1 className="title-font sm:text-4xl text-xl mb-4 font-medium text-blue-600">
                         Hi, my name is Chad.<br/>
                     </h1>
-                    <TypeAnimation className="title-font sm:text-4xl text-xl mb-4 font-medium text-gray-800 whitespace-nowrap"
+                    <TypeAnimation className="title-font sm:text-4xl md:text-l mb-4 font-medium text-gray-800 whitespace-nowrap"
                         sequence={[
                             "I'm a mechanic.",
                             1000,
@@ -43,21 +43,21 @@ const Home = () => {
                     <p className="text-gray-500 mb-8 sm:text-xl leading-relaxed text-l md:w-full w-7/8">
                         Starting in the heavy equipment industry, I have transitioned into the captivating world of software engineering. My work experience has honed my problem-solving abilities, collaborative skill set, and keen eye for detail; while my education has instilled in me a results-driven and analytical approach. Such qualities facilitate my commitment to crafting robust software solutions while improving lives within society. I'm eager to join a new team that values quality, creativity, and learning. If our values align, let's thrive together!
                     </p>
-                    <div className="flex md:flex-col sm:flex-col lg:flex-row flex-wrap justify-center items-center text-lg">
+                    <div className="flex md:flex-row flex-col lg:flex-row justify-center items-center text-lg">
                         <a
                             href="/Resume_Chad_Holst.pdf"
                             download="Resume_Chad_Holst.pdf"
-                            className="mb-4 text-white bg-blue-600 border-0 py-2 px-6 focus:outline-none hover:bg-blue-400 hover:text-slate-800 rounded-md font-bold text-lg mb-2 text-center">
+                            className="mb-4 text-white bg-blue-600 border-0 py-3 px-4 focus:outline-none hover:bg-blue-400 hover:text-slate-800 rounded-md font-bold text-lg mb-2 text-center">
                             Download Resume
                         </a>
                         <Link
                             to="/projects"
-                            className="mb-4 md:ml-4 sm:ml-4 text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-600 hover:text-blue-600 rounded-md font-bold text-lg mb-2 text-center">
+                            className="mb-4 md:ml-4 sm:ml-4 text-gray-400 bg-gray-800 border-0 py-3 px-4 focus:outline-none hover:bg-gray-600 hover:text-blue-600 rounded-md font-bold text-lg mb-2 text-center">
                             View My Work
                         </Link>
                         <Link
                             to="/contact"
-                            className="mb-4 md:ml-4 text-white bg-blue-600 border-0 py-2 px-6 focus:outline-none hover:bg-blue-400 hover:text-slate-800 rounded-md font-bold text-lg mb-2 text-center">
+                            className="mb-4 md:ml-4 text-white bg-blue-600 border-0 py-3 px-4 focus:outline-none hover:bg-blue-400 hover:text-slate-800 rounded-md font-bold text-lg mb-2 text-center">
                             Let's Collaborate
                         </Link>
                     </div>
