9332a3ad28131a55e42c08ba78471443ec78f810
HolstCh <chad.holst@Hotmail.com>
2024-04-06 19:34:52 -0600

    extends drone project information/images to match repository

--- my-app/src/components/Projects.js
+++ my-app/src/components/Projects.js
@@ -84,13 +84,95 @@ export default function Projects() {
                             <h2 className="text-md lg:text-xl title-font font-medium text-center text-white mb-3">Project Description</h2>
                             <p className="text-base text-gray-400 leading-relaxed mb-3">{selectedProject.description}</p>
                             {selectedProject.id === 1 ?
-                                <div className="items-center flex flex-col">
-                                    <h2 className="text-md lg:text-xl title-font font-medium text-center text-white mb-3">Testimonials</h2>
+                                <div>
+                                    <h2 className="text-md lg:text-xl title-font font-medium text-center text-white mb-3">Software System</h2>
                                     <img
                                         alt="drone team"
                                         className="object-cover object-center mb-4 rounded-md"
-                                        src={"../images/robotics.jpg"}
+                                        src={"../images/software_architecture.jpg"}
                                     />
+                                    <h2 className="text-sm lg:text-lg title-font font-medium text-gray-400 mb-3">
+                                        <h2 className="title-font font-medium mb-2 text-sm lg:text-lg">The <span className="text-blue-400">Software Architecture</span> contains the following modules:</h2>
+                                        <ul className="list-disc pl-5">
+                                            <li><span className="text-blue-400">Model:</span>  contains logic, calculations, and direct PX4 communication </li>
+                                            <li><span className="text-blue-400">View:</span>  user interface to display data and receive commands from the user </li>
+                                            <li><span className="text-blue-400">Controller:</span>  processes command data and relays data between Model and View </li>
+                                            <li><span className="text-blue-400">Client/Server:</span>  architecture between Controller and Model modules </li>
+                                        </ul>
+                                    </h2>
+                                    <img
+                                        alt="drone team"
+                                        className="object-cover object-center mb-4 rounded-md"
+                                        src={"../images/GCS.png"}
+                                    />
+                                    <div className="text-sm lg:text-lg text-gray-400 mb-3">
+                                        <h2 className="title-font font-medium mb-2">The <span className="text-blue-400">Ground Control System (GCS)</span> has seven user interface components:</h2>
+                                        <ul className="list-disc pl-5">
+                                            <li>Thruster angle sliders</li>
+                                            <li>Thruster RPM sliders</li>
+                                            <li>Open/close gripper sliders</li>
+                                            <li>Reset State button</li>
+                                            <li>Command Mode dropdown (Single or Multiple)</li>
+                                            <li>Send Commands button</li>
+                                            <li>IMU data display (acceleration and angular speed in x, y, and z directions)</li>
+                                        </ul>
+                                    </div>
+                                    <h2 className="text-md lg:text-xl title-font font-medium text-center text-white mb-3">Electronic System</h2>
+                                    <img
+                                        alt="drone team"
+                                        className="object-cover object-center mb-4 rounded-md"
+                                        src={"../images/pin_diagram.png"}
+                                    />
+                                    <table className="table-auto w-full mb-3">
+                                        <thead>
+                                        <tr>
+                                            <th className="px-4 py-2 text-blue-400">Component</th>
+                                            <th className="px-4 py-2 text-blue-400">Rating</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        <tr className="border-b border-gray-200">
+                                            <td className="px-4 py-2">Seabotix Thruster</td>
+                                            <td className="px-4 py-2">110W Max, 80W Continuous</td>
+                                        </tr>
+                                        <tr className="border-b border-gray-200">
+                                            <td className="px-4 py-2">Hitec HS422 Servo Motor</td>
+                                            <td className="px-4 py-2">5W (4.8-6V, 0.8A)</td>
+                                        </tr>
+                                        <tr className="border-b border-gray-200">
+                                            <td className="px-4 py-2">Subsea Gripper</td>
+                                            <td className="px-4 py-2">108W</td>
+                                        </tr>
+                                        <tr className="border-b border-gray-200">
+                                            <td className="px-4 py-2">Pixhawk 4</td>
+                                            <td className="px-4 py-2">12.5W USB Supply</td>
+                                        </tr>
+                                        <tr className="border-b border-gray-200">
+                                            <td className="px-4 py-2">Pololu Motor Controller</td>
+                                            <td className="px-4 py-2">15W</td>
+                                        </tr>
+                                        <tr className="border-b border-gray-200">
+                                            <td className="px-4 py-2">HolyBro PM07-V21 Board</td>
+                                            <td className="px-4 py-2">45W</td>
+                                        </tr>
+                                        <tr className="border-b border-gray-200">
+                                            <td className="px-4 py-2">Raspberry Pi 3B</td>
+                                            <td className="px-4 py-2">12.5W USB Supply</td>
+                                        </tr>
+                                        <tr className="border-b border-gray-200">
+                                            <td className="px-4 py-2">BlueRobotics LiPo Battery</td>
+                                            <td className="px-4 py-2">14.8V, 15.6Ah</td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                    <div className="items-center flex flex-col">
+                                        <h2 className="text-md lg:text-xl title-font font-medium text-center text-white mb-3">Testimonials</h2>
+                                        <img
+                                            alt="drone team"
+                                            className="object-cover object-center mb-4 rounded-md"
+                                            src={"../images/robotics.jpg"}
+                                        />
+                                    </div>
                                 </div>
                                 : null}
                             {selectedProject.id === 1 ? selectedProject.testimonials.map((testimonial) => (<p className="text-base text-gray-400 leading-relaxed mb-3">{testimonial}</p>)) : null}
