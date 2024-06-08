5341df01fda0340998875443dc31e4ed8eb0513b
HolstCh <chad.holst01@gmail.com>
2024-06-08 12:54:16 -0600

    Adds unit tests for App.js. Refactors routing in App.js and Layout.js.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,9 @@
-import Footer from "./components/Footer";
 import Projects from "./components/Projects";
-import Project from "./components/Project"
 import Home from "./components/Home";
 import Contact from "./components/Contact";
 import Layout from "./components/Layout";
 import './styles/NavBar.css';
-import React, {useEffect} from "react"
+import React from "react"
 import {Routes, Route} from "react-router-dom";
 
 function App() {
@@ -15,11 +13,9 @@ function App() {
         <Layout/>
         <div className="bg-white">
             <Routes>
-                <Route path="/" element={<Layout />} />
-                    <Route index element={<Home />} />
-                    <Route path="home" element={<Home />} />
-                    <Route path="projects" element={<Projects />} />
-                    <Route path="contact" element={<Contact />} />
+                <Route path="/" element={<Home />} />
+                <Route path="projects" element={<Projects />} />
+                <Route path="contact" element={<Contact />} />
             </Routes>
         </div>
       </>
@@ -27,5 +23,3 @@ function App() {
 }
 
 export default App;
-
-/*  <Route path="project/1" element={<Project id={1} />} /> */
--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,8 +1,33 @@
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { BrowserRouter } from 'react-router-dom'; // or MemoryRouter
 import App from './App';
+import Contact from './components/Contact'
+import Projects from './components/Projects'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// checks routes within App component
+describe('App Unit Tests', () => {
+
+  test('renders home page', () => {
+    render(
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+    );
+    expect(screen.getByText('Hi, my name is Chad.')).toBeInTheDocument();
+  });
+
+  test('renders projects page', () => {
+    render(<Projects />);
+    expect(screen.getByText('The following are samples of projects that I have worked on in the past. Clicking on a project will provide a description.')).toBeInTheDocument()
+  });
+
+  test('renders contact page', () => {
+    render(<Contact />);
+    expect(screen.getByText('Name')).toBeInTheDocument();
+    expect(screen.getByText('Message')).toBeInTheDocument();
+    expect(screen.getByText('Email')).toBeInTheDocument();
+
+  });
+
 });
